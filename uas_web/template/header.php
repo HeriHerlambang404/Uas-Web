f12e5aea1bd38b0925ecae6ba1458c83d7f8a301
HeriHerlambang404 <heriherlambang098@gmail.com>
2026-01-08 16:58:54 +0700

    Change title and navbar brand to 'E - ARTIKEL'

--- uas_web/template/header.php
+++ uas_web/template/header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="id">
 <head>
-    <title>UAS WEBSITE</title>
+    <title>E - ARTIKEL</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
         body { display: flex; min-height: 100vh; flex-direction: column; }
@@ -12,8 +12,9 @@
 </head>
 <body>
 <nav class="navbar navbar-dark bg-dark px-3">
-    <span class="navbar-brand">UAS WEBSITE</span>
+    <span class="navbar-brand">E - ARTIKEL</span>
 </nav>
 <div class="wrapper">
     <?php include "sidebar.php"; ?>
+
     <main>
